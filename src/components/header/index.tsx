2fa0e7c171f402d7e9314b9272a169cd8a31a031
Suraj Vijay <surajcpp115@gmail.com>
2022-07-16 12:59:16 +0530

    dark theme for header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,23 +1,27 @@
-import {menuTranslations} from '../../utils/translations.util';
-import {Container, Signature, CurrentPage} from './styles';
+import { menuTranslations } from '../../utils/translations.util';
+import { Container, Signature, CurrentPage } from './styles';
 
 import Menu from '../menu';
 
-import {useCommonContext} from '../../contexts/common.context';
-import {useMenuContext} from '../../contexts/menu.context';
+import { useCommonContext } from '../../contexts/common.context';
+import { useMenuContext } from '../../contexts/menu.context';
 
 interface HeaderProps {}
 
 const Header: React.FC<HeaderProps> = () => {
-  const {language} = useCommonContext();
+  const {language, isDark} = useCommonContext();
   const {menu} = useMenuContext();
 
   const pageName: string = Object.values(menuTranslations)[menu][+language];
 
   return (
     <Container>
-      <Signature>Suraj Vijay</Signature>
-      <CurrentPage key={pageName}>{pageName}</CurrentPage>
+      <Signature key={isDark + ""} dark={isDark}>
+        Suraj Vijay
+      </Signature>
+      <CurrentPage key={pageName + `${isDark}`} dark={isDark}>
+        {pageName}
+      </CurrentPage>
       <div />
       <Menu />
     </Container>
--- src/components/header/styles.tsx
+++ src/components/header/styles.tsx
@@ -9,23 +9,25 @@ export const Container = styled.div`
   padding: 0vw 0.49rem 0vw 0.49rem;
 `;
 
-export const Signature = styled.span`
+export const Signature = styled.span<{dark?: boolean}>`
   font-family: signature;
   font-size: 1.4rem;
-  color: #1a1a1a;
   font-weight: 600;
   cursor: pointer;
   letter-spacing: 0.12rem;
   word-spacing: 0.24rem;
+  color: #1a1a1a;
+  ${props => props.dark && `color: #f5f4f9;`}
   &:hover {
     opacity: 0.7;
     font-weight: 100;
   }
 `;
 
-export const CurrentPage = styled.span`
+export const CurrentPage = styled.span<{dark?: boolean}>`
   font-family: calibri;
   color: #1a1a1a;
+  ${props => props.dark && `color: #f5f4f9;`}
   font-size: 1.8rem;
   animation: ${FadeAnim1} 1s;
   text-transform: capitalize;
