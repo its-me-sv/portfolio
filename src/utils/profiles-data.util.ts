2b4b1a8f0956861604e9e3ec91c6704a9b6ddcf3
Suraj Vijay <surajcpp115@gmail.com>
2022-07-19 13:22:51 +0530

    added profiles url and images link

---
+++ src/utils/profiles-data.util.ts
@@ -0,0 +1,25 @@
+interface Profile {
+  icon: string;
+  name: string;
+  url: string;
+}
+
+export const profileDetails: Array<Profile> = [
+  { name: 'GitHub', url: 'https://github.com/its-me-sv', icon: 'https://res.cloudinary.com/djeteilo6/image/upload/v1657342048/portfolio/profiles/github_t8grvj.png' },
+  { name: 'FreeCodeCamp', url: 'https://www.freecodecamp.org/sura_vijay_115_py', icon: 'https://res.cloudinary.com/djeteilo6/image/upload/v1657342047/portfolio/profiles/freecodecamp_dbmnui.jpg' },
+  { name: 'HackerRank', url: 'https://www.hackerrank.com/surajcpp115', icon: 'https://res.cloudinary.com/djeteilo6/image/upload/v1657342047/portfolio/profiles/hackerrank_bcj1v7.jpg' },
+  { name: 'Project Euler', url: 'https://projecteuler.net/progress=ImSurajVijay115', icon: 'https://res.cloudinary.com/djeteilo6/image/upload/v1657342048/portfolio/profiles/project_euler_qwdpb1.png' },
+  { name: 'CodeChef', url: 'https://www.codechef.com/users/suraj_vijay115', icon: 'https://res.cloudinary.com/djeteilo6/image/upload/v1657342046/portfolio/profiles/code_chef_w38bmw.png' },
+  { name: 'LinkedIn', url: 'https://www.linkedin.com/in/suraj-vijay-32b679224', icon: 'https://res.cloudinary.com/djeteilo6/image/upload/v1657342048/portfolio/profiles/linkedin_nkhuuu.png' },
+  { name: 'Twitter', url: 'https://twitter.com/surajvijay67', icon: 'https://res.cloudinary.com/djeteilo6/image/upload/v1657342048/portfolio/profiles/twitter_f0urt0.png' },
+  { name: 'HashNode', url: 'https://surajvijay.hashnode.dev/', icon: 'https://res.cloudinary.com/djeteilo6/image/upload/v1657342048/portfolio/profiles/hashnode_r6bwbk.png' },
+  { name: 'Daily.dev', url: 'https://app.daily.dev/dark_knight_67', icon: 'https://res.cloudinary.com/djeteilo6/image/upload/v1657342046/portfolio/profiles/dailydev_zzgrk5.jpg' },
+  { name: 'Instagram', url: 'https://www.instagram.com/suraj_vijay_115.py/', icon: 'https://res.cloudinary.com/djeteilo6/image/upload/v1657342048/portfolio/profiles/instagram_fjnozs.png' },
+  { name: 'YouTube', url: 'https://www.youtube.com/channel/UCjzb3NLEbS_amCj1mhhiu2w', icon: 'https://res.cloudinary.com/djeteilo6/image/upload/v1657342048/portfolio/profiles/youtube_o424xk.png' },
+  { name: 'Instagram', url: 'https://www.instagram.com/purely_python.py/', icon: 'https://res.cloudinary.com/djeteilo6/image/upload/v1657342048/portfolio/profiles/instagram_fjnozs.png' },
+  { name: 'YouTube', url: 'https://www.youtube.com/channel/UCzaYKv9poDcmO9J_Yg1dj5Q', icon: 'https://res.cloudinary.com/djeteilo6/image/upload/v1657342048/portfolio/profiles/youtube_o424xk.png' },
+  { name: 'Discord', url: 'https://discord.com/channels/@me/711266358343762062', icon: 'https://res.cloudinary.com/djeteilo6/image/upload/v1657342047/portfolio/profiles/discord_ipplfb.png' },
+  { name: 'FaceBook', url: 'https://www.facebook.com/vijay.suraj.14/', icon: 'https://res.cloudinary.com/djeteilo6/image/upload/v1657342047/portfolio/profiles/facebook_wlols3.png' },
+  { name: 'COD - Warzone', url: 'https://cod.tracker.gg/warzone/profile/battlenet/SurajVijay11%231906/overview', icon: 'https://res.cloudinary.com/djeteilo6/image/upload/v1657342048/portfolio/profiles/warzone_ml4fev.png' },
+  { name: 'GTA V', url: 'https://socialclub.rockstargames.com/member/gamersuraj115/games/gtav/pc/career/overview/gtaonline', icon: 'https://res.cloudinary.com/djeteilo6/image/upload/v1657342048/portfolio/profiles/grand-theft-auto-v-1_q9mq4d.png' },
+];
