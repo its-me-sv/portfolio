1e61f6ea1762c44ab83bb83142bc76b77475682b
Suraj Vijay <surajcpp115@gmail.com>
2022-07-23 10:57:07 +0530

    project card with light and dark theme

--- src/components/projects/project-card.component.tsx
+++ src/components/projects/project-card.component.tsx
@@ -1,10 +1,122 @@
+import { useState, useEffect } from 'react';
+
+import {
+  Card,
+  CardTop,
+  Gallery,
+  SliderItem,
+  Description,
+  TechStack,
+  HrzntlLn,
+  CardBottom
+} from "./styles";
+
+import codeIcon from '../../assets/icons/code.png';
+import openIcon from '../../assets/icons/open.png';
+
+import likeIcon from '../../assets/icons/like.png';
+import commentIcon from '../../assets/icons/comment.png';
+import shareIcon from '../../assets/icons/share.png';
+
+import { useCommonContext } from '../../contexts/common.context';
+
+const dummyImages: Array<string> = [
+  "https://res.cloudinary.com/djeteilo6/image/upload/v1657349877/portfolio/projects/%20Stone_Paper_Scissors/p2_m09p2l.png",
+  "https://res.cloudinary.com/djeteilo6/image/upload/v1657349877/portfolio/projects/%20Stone_Paper_Scissors/p3_ewimbe.png",
+  "https://res.cloudinary.com/djeteilo6/image/upload/v1657349877/portfolio/projects/%20Stone_Paper_Scissors/p1_kcjmc5.png",
+  "https://res.cloudinary.com/djeteilo6/image/upload/v1657349877/portfolio/projects/%20Stone_Paper_Scissors/p4_nq1qo8.png",
+];
+
+const skills: Array<string> = [
+  'HTML', 'CSS', 'JavaScript',
+  'React', 'Redux', 'Mongodb',
+  'Cassandra', 'Redis', 'Python',
+  'HTML', 'CSS', 'JavaScript',
+  'React', 'Redux', 'Mongodb',
+  'Cassandra', 'Redis', 'Python',
+  'HTML', 'CSS', 'JavaScript',
+  'React', 'Redux', 'Mongodb',
+  'Cassandra', 'Redis', 'Python',
+  'Cassandra', 'Redis', 'Python',
+  'HTML', 'CSS', 'JavaScript',
+  'React', 'Redux', 'Mongodb',
+  'Cassandra', 'Redis', 'Python',
+  'HTML', 'CSS', 'JavaScript',
+  'React', 'Redux', 'Mongodb',
+  'Cassandra', 'Redis', 'Python',
+  'Cassandra', 'Redis', 'Python',
+  'HTML', 'CSS', 'JavaScript',
+  'React', 'Redux', 'Mongodb',
+  'Cassandra', 'Redis', 'Python',
+  'HTML', 'CSS', 'JavaScript',
+  'React', 'Redux', 'Mongodb',
+  'Cassandra', 'Redis', 'Python',
+];
+
 interface ProjectCardProps {}
 
 const ProjectCard: React.FC<ProjectCardProps> = () => {
+  const { isDark } = useCommonContext();
+  
+  const [currImage, setCurrImage] = useState<number>(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrImage(prev => (prev + 1) % dummyImages.length);
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div>
-      
-    </div>
+    <Card dark={isDark}>
+      <CardTop dark={isDark}>
+        <span>Project Title</span>
+        <div>
+          <img src={codeIcon} alt="code" />
+          <img src={openIcon} alt="open" />
+        </div>
+      </CardTop>
+      <Gallery dark={isDark}>
+        <img
+          key={"image " + (currImage + 1 + "")}
+          src={dummyImages[currImage]}
+          alt={"image " + (currImage + 1 + "")}
+        />
+        <div>
+          {dummyImages.map((val, idx) => (
+            <SliderItem
+              key={val}
+              selected={idx === currImage}
+              onClick={() => setCurrImage(idx)}
+              dark={isDark}
+            />
+          ))}
+        </div>
+      </Gallery>
+      <Description dark={isDark}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec
+        tempus quam, id porta velit. Integer facilisis dolor ac arcu imperdiet,
+        eget feugiat arcu vestibulum. Aenean ultricies erat ac odio egestas
+        aliquet dictum ante id pharetra. Nulla vulputate dignissim libero
+      </Description>
+      <HrzntlLn dark={isDark} />
+      <TechStack dark={isDark}>Tech stack: {skills.join(", ")}</TechStack>
+      <HrzntlLn dark={isDark} />
+      <CardBottom dark={isDark}>
+        <div>
+          <img src={likeIcon} alt="like" />
+          <span>7</span>
+        </div>
+        <div>
+          <img src={commentIcon} alt="comment" />
+          <span>34</span>
+        </div>
+        <div>
+          <img src={shareIcon} alt="share" />
+          <span>4</span>
+        </div>
+      </CardBottom>
+    </Card>
   );
 };
 
--- src/components/projects/styles.tsx
+++ src/components/projects/styles.tsx
@@ -1,14 +1,143 @@
 import styled from 'styled-components';
+import { 
+  FadeAnim1, 
+  LightBoxShadow,
+  DarkBoxShadow
+} from '../../utils/styles.util';
 
 export const Container = styled.div`
   overflow: auto;
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  grid-row-gap: 1.4rem;
+  grid-row-gap: 2.4rem;
   place-items: center;
   padding: 1.4% 0%;
 `;
 
-export const Card = styled.div`
+export const Card = styled.div<{dark: boolean}>`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 28vw;
+  gap: 0.42rem;
+  padding: 1% 1.6%;
+  border-radius: 0.42rem;
+  animation: ${FadeAnim1} 1s;
+  ${LightBoxShadow};
+  ${props => props.dark && DarkBoxShadow}
+`;
 
+export const CardTop = styled.div<{dark: boolean}>`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  span {
+    font-family: geomanist;
+    font-size: 1.6rem;
+    color: #1a1a1a;
+    ${props => props.dark && `color: #f5f4f9`}
+  }
+  div {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 0.42rem;
+    img {
+      width: 2.1rem;
+      height: auto;
+      opacity: 0.8;
+      cursor: pointer;
+      &:hover {opacity: 0.5;}
+      ${props => props.dark && `filter: invert(100%);`}
+    }
+  }
+`;
+
+export const Gallery = styled.div<{dark: boolean}>`
+  display: flex;
+  flex-direction: column;
+  gap: 0.42rem;
+  img {
+    width: 98%;
+    height: 24vh;
+    border-radius: 0.3rem;
+    animation: ${FadeAnim1} 1s;
+    border: 1px solid #1a1a1a;
+    ${(props) => props.dark && `border: 1px solid #f5f4f9;`}
+  }
+  div {
+    display: flex;
+    align-items: center;
+    align-self: center;
+    gap: 0.3rem;
+  }
+`;
+
+export const SliderItem = styled.span<{selected: boolean; dark: boolean}>`
+  width: 0.5rem;
+  height: 0.5rem;
+  border-radius: 50%;
+  cursor: pointer;
+  border: 1px solid #1a1a1a;
+  ${props => props.selected && `background-color: #1a1a1a;`}
+  ${props => props.dark && `
+    border: 1px solid #f5f4f9;
+    ${props.selected && `background-color: #f5f4f9;`}
+  `}
+`;
+
+export const Description = styled.div<{dark: boolean}>`
+  font-family: arial;
+  font-size: 1.2rem;
+  max-height: 24vh;
+  overflow: auto;
+  color: #1a1a1a;
+  ${props => props.dark && `color: #f5f4f9;`}
+`;
+
+export const TechStack = styled.span<{dark: boolean}>`
+  font-family: bahnschrift;
+  font-size: 1.2rem;
+  opacity: 0.9;
+  max-height: 7vh;
+  overflow: auto;
+  color: #1a1a1a;
+  ${props => props.dark && `color: #f5f4f9;`}
+`;
+
+export const HrzntlLn = styled.span<{dark: boolean}>`
+  height: 0.2rem;
+  width: 100%;
+  align-self: center;
+  border-radius: 0.42rem;
+  opacity: 0.5;
+  background-color: #1a1a1a;
+  ${props => props.dark && `background-color: #f5f4f9;`}
+`;
+
+export const CardBottom = styled.div<{dark: boolean}>`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  div {
+    display: flex;
+    align-items: center;
+    gap: 0.3rem;
+    img {
+      width: 2.1rem;
+      height: auto;
+      opacity: 0.9;
+      cursor: pointer;
+      ${props => props.dark && `filter: invert(100%);`}
+      &:hover {opacity: 0.5;}
+    }
+    span {
+      font-family: arial;
+      font-size: 1.2rem;
+      color: #1a1a1a;
+      ${props => props.dark && `color: #f5f4f9;`}
+    }
+  }
 `;
