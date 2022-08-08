6ac8c771f7174f9a22649195ba27e712c704f545
Suraj Vijay <surajcpp115@gmail.com>
2022-08-08 20:39:10 +0530

    fetching more projects when reaching end

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -1,26 +1,28 @@
-import { Container, LoadMore } from './styles';
-import { projectsPageTranslations } from '../../utils/translations.util';
+import { useCallback, useRef } from 'react';
+import { Container } from './styles';
 
 import ProjectCard from './project-card.component';
 import { useProjectContext } from '../../contexts/project.context';
-import { useCommonContext } from '../../contexts/common.context';
 
 interface ProjectsProps {}
 
 const Projects: React.FC<ProjectsProps> = () => {
-  const { isDark, language } = useCommonContext();
-  const { projectIds, fetchProjects, currPage } = useProjectContext();
+  const { projectIds, fetchProjects } = useProjectContext();
+
+  const scrollRef = useRef<HTMLDivElement>() as React.RefObject<HTMLDivElement>;
+
+  const onScroll = useCallback(() => {
+    if (!scrollRef.current) return;
+    const { scrollTop, scrollHeight, clientHeight } = scrollRef.current;
+    if (scrollTop + clientHeight === scrollHeight)
+      fetchProjects!();
+  }, [fetchProjects]);
 
   return (
-    <Container>
+    <Container ref={scrollRef} onScroll={onScroll}>
       {projectIds.map((projectId) => (
         <ProjectCard key={projectId} id={projectId} />
       ))}
-      {currPage !== null && (
-        <LoadMore onClick={fetchProjects} dark={isDark}>
-          {projectsPageTranslations.ldPrj[+language]}
-        </LoadMore>
-      )}
     </Container>
   );
 };
