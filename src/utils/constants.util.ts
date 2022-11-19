4579719b9fa620e2a6dabf94b0a4ae21ed637edc
Suraj Vijay <surajcpp115@gmail.com>
2022-11-19 10:10:33 +0530

    configured api url for dev and prod

--- src/utils/constants.util.ts
+++ src/utils/constants.util.ts
@@ -1,1 +1,3 @@
-export const API_URL = `https://suraj-vijayan-portfolio-backend.onrender.com`;
+export const API_URL = process.env.NODE_ENV === "development" 
+? `http://127.0.0.1:5000`
+: `https://suraj-vijayan-portfolio-backend.onrender.com`;
