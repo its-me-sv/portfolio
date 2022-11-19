cb0377434361d290d2569bcc461437b072bb3634
Suraj Vijay <surajcpp115@gmail.com>
2022-11-19 11:12:01 +0530

    intial config in loader component

--- src/components/loader/index.tsx
+++ src/components/loader/index.tsx
@@ -1,17 +1,19 @@
 import { Container, SignatureFont, InfoText } from "./styles";
 
 interface LoaderProps {
-  initial?: boolean
+  initial?: boolean;
 }
 
 const Loader: React.FC<LoaderProps> = ({initial}) => {
   return (
     <Container>
       <SignatureFont>Suraj Vijayan</SignatureFont>
-      <InfoText>
-        Connecting to the back-end server. This may take a 
-        few moments, and yes, this site is full stack.
-      </InfoText>
+      {(initial && initial === true) && (
+        <InfoText>
+          Connecting to the back-end server. This may take a 
+          few moments, and yes, this site is full stack.
+        </InfoText>
+      )}
     </Container>
   );
 };
