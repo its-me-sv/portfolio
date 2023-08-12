89a4664c0eed2e8fe87ac42a304a8094d524ca72
Suraj Vijay <surajcpp115@gmail.com>
2023-08-12 14:39:19 +0530

    updated backend url to gcp app engine

--- src/utils/constants.util.ts
+++ src/utils/constants.util.ts
@@ -1,3 +1,3 @@
 export const API_URL = process.env.NODE_ENV === "development" 
 ? `http://192.168.29.97:5000`
-: `https://suraj-vijayan-portfolio-backend.onrender.com`;
+    : `https://portfolio-backend-dot-civil-cab-393407.el.r.appspot.com`;
